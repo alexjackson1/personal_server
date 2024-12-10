793f080d44bb5c8fd6cdc633cd2bb2ff0c148f76
Alex Jackson <mail@alexjackson.uk>
2024-12-10 22:26:54 +0000

    Update launch script

--- launch.sh
+++ launch.sh
@@ -4,5 +4,8 @@ set -e
 
 export $(grep -vE '^\s*#' .env | grep -vE '^\s*$' | xargs)
 
+mkdir -p /mnt/ajcloud/pgadmin_data
+sudo chown -R 5050:5050 /mnt/ajcloud/pgadmin_data
+
 envsubst < servers.json.template > servers.json
 docker compose up -d
